35ed366777433f907da87022e00b9eb41e880b15
Nathaniel Nelson <nelson.nleroy@gmail.com>
2013-02-11 21:22:02 -0700

    yeah

---
+++ SpaceHordes/SpaceHordes/Entities/Systems/ExplosionSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameLibrary.Dependencies.Entities;
+using GameLibrary.Entities.Components;
+
+namespace SpaceHordes.Entities.Systems
+{
+    public class ExplosionSystem : GroupSystem
+    {
+        public ExplosionSystem()
+            : base("Explosions")
+        {
+        }
+
+        public override void Process(Entity e)
+        {
+            Animation a = e.GetComponent<Animation>();
+
+            if (a.Type == AnimationType.None)
+            {
+                e.Delete();
+            }
+        }
+    }
+}
