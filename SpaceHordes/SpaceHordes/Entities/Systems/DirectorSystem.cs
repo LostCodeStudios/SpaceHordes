0700a044a14023ac93759638724afbd6092041c7
Nathaniel Nelson <nelson.nleroy@gmail.com>
2013-02-15 16:59:11 -0700

    spawn rate successfully nerfed

--- SpaceHordes/SpaceHordes/Entities/Systems/DirectorSystem.cs
+++ SpaceHordes/SpaceHordes/Entities/Systems/DirectorSystem.cs
@@ -17,15 +17,22 @@ namespace SpaceHordes.Entities.Systems
         int difficulty = 0;
 
         //Start off with a minute worth of time so spawns don't delay by a minute due to casting
-        float elapsedSeconds = 60f;
-        float elapsedMinutes = 1f;
+        float elapsedSeconds;
+        float elapsedMinutes;
 
         int playerDeaths = 0;
         int baseDamage = 0;
 
+        int mooksToSpawn = 0;
+        int gunnersToSpawn = 0;
+        int huntersToSpawn = 0;
+        int destroyersToSpawn = 0;
+
         public DirectorSystem()
             : base(500)
         {
+            elapsedSeconds = 60f;
+            elapsedMinutes = 1f;
         }
 
         public void LoadContent(Entity Base)
@@ -37,20 +44,35 @@ namespace SpaceHordes.Entities.Systems
         {
             base.ProcessEntities(entities);
 
-            int milliseconds = 500;
+            elapsedSeconds += .5f;
+            elapsedMinutes += .5f/60f;
 
-            elapsedSeconds += milliseconds/1000;
-            elapsedMinutes += milliseconds/60000;
-
-            difficulty = (int)(elapsedMinutes) - ((int)(playerDeaths / 2) + baseDamage);
+            difficulty = (int)(elapsedMinutes - (playerDeaths / 2 + baseDamage));
 
             //Spawn mooks per second equal to Difficulty
-            if (elapsedSeconds % 1 == 0)
+            if (elapsedSeconds % 5 == 0)
             {
-                for (int i = 0; i < difficulty; i++)
+                mooksToSpawn = 3 * difficulty;
+                gunnersToSpawn = (int)(difficulty / 3);
+                huntersToSpawn = (int)(difficulty / 5);
+                destroyersToSpawn = (int)(difficulty / 10);
+
+                for (int i = 0; i < mooksToSpawn; i++)
                 {
                     int type = r.Next(9);
                     World.CreateEntity("Mook", type, Base.GetComponent<Body>()).Refresh();
+                }
+
+                for (int i = 0; i < gunnersToSpawn; i++)
+                {
+                }
+
+                for (int i = 0; i < huntersToSpawn; i++)
+                {
+                }
+
+                for (int i = 0; i < destroyersToSpawn; i++)
+                {
                 }
             }
         }
