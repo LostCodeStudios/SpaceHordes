ebb9622c3b3e568d2bdcb2f8b38d25655446db9a
William Guss <carlostnb@gmail.com>
2013-02-19 14:25:42 -0700

    Tits

--- SpaceHordes/SpaceHordes/Entities/Systems/BulletRemovalSystem.cs
+++ SpaceHordes/SpaceHordes/Entities/Systems/BulletRemovalSystem.cs
@@ -35,8 +35,8 @@ namespace SpaceHordes.Entities.Systems
             if (e.Group == "Bullets") //If bullet has component
             {
                 Particle t = TransformMapper.Get(e);
-                if ((t.Position.X > camera.ConvertScreenToWorld(new Vector2(1280, 720)).X
-                    || t.Position.Y > camera.ConvertScreenToWorld(new Vector2(1280, 720)).Y ||
+                if ((t.Position.X > camera.ConvertScreenToWorld(new Vector2(2)*ScreenHelper.Center).X
+                    || t.Position.Y > camera.ConvertScreenToWorld(new Vector2(2) * ScreenHelper.Center).Y ||
                     (t.Position.X < camera.ConvertScreenToWorld(new Vector2(0, 0)).X
                     || t.Position.Y < camera.ConvertScreenToWorld(new Vector2(0, 0)).Y)))
                 {
