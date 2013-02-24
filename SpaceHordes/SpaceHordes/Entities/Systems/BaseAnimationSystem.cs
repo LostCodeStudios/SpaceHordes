9bfb28f3e728fa06f110f69198de325a36ab7b06
Nathaniel Nelson <nelson.nleroy@gmail.com>
2013-02-24 16:27:45 -0700

    Base destruction animation woot woot fuck yeah bitch

--- SpaceHordes/SpaceHordes/Entities/Systems/BaseAnimationSystem.cs
+++ SpaceHordes/SpaceHordes/Entities/Systems/BaseAnimationSystem.cs
@@ -5,6 +5,8 @@ using System.Text;
 using GameLibrary.Dependencies.Entities;
 using GameLibrary.Entities.Components.Physics;
 using GameLibrary.Helpers;
+using GameLibrary.Entities.Components;
+using SpaceHordes.Entities.Components;
 
 namespace SpaceHordes.Entities.Systems
 {
@@ -25,8 +27,9 @@ namespace SpaceHordes.Entities.Systems
 
         public override void Process(Entity e)
         {
+            #region Float Effect
+
             Body b = e.GetComponent<Body>();
-  
 
             if (ConvertUnits.ToDisplayUnits(b.Position.Y) >= _MaxH)
                 _Direction = -_Speed;
@@ -35,6 +38,32 @@ namespace SpaceHordes.Entities.Systems
 
 
             b.Position += new Microsoft.Xna.Framework.Vector2(b.Position.X - _X,ConvertUnits.ToSimUnits(_Direction));
+
+            #endregion
+
+            #region Sprite Animation
+
+            Sprite s = e.GetComponent<Sprite>();
+            Health h = e.GetComponent<Health>();
+
+            if (h.IsAlive)
+            {
+                e.RemoveComponent<Sprite>(s);
+
+                double healthFraction = (h.CurrentHealth / h.MaxHealth);
+
+                int frame = 0;
+                if (healthFraction < 0.66)
+                    frame = 1;
+                if (healthFraction < 0.33)
+                    frame = 2;
+
+                s.FrameIndex = frame;
+                
+                e.AddComponent<Sprite>(s);
+            }
+
+            #endregion
         }
     }
 }
